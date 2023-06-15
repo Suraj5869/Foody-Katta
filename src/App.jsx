87636d5974f03344ee879a2557af6fc94ac5eaf1
Suraj <suraj.sakhare5869@gmail.com>
2023-06-15 22:02:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+// import menu from './menu.html'
+
+export default App
